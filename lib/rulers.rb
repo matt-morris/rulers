de4c77c68c70271977b95c4bc887e6b135825bc4
Matt Morris <double.emms@gmail.com>
2015-06-27 23:26:20 -0600

    add controller class and require routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,25 @@
 require "rulers/version"
+require "rulers/routing"
 require "rulers/array"
 
 module Rulers
   class Application
     def call(env)
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
       [200, {'Content-Type' => 'text/html'},
-        ["You've got Rulers"]]
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
