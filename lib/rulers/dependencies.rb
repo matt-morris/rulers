613b8e4e708a03109e3a83c0fd9b9b46d3f416c8
Matt Morris <double.emms@gmail.com>
2015-07-01 21:30:01 -0600

    slightly robustify magical autoloader

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,12 @@
 class Object
   def self.const_missing(c)
+    return nil if @calling_const_missing
+
+    @calling_const_missing = true
     require c.to_s.to_snake_case
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
